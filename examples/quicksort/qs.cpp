6e7298fec97a1b4b80f95d6472434b5b7aad52ec
massimot <massimot@5b2d11e7-8b83-46bf-b187-68a94c616a7d>
2010-10-03 16:34:07 +0000

    added the Quicksort example

---
+++ examples/quicksort/qs.cpp
@@ -0,0 +1,156 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* ***************************************************************************
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as 
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  As a special exception, you may use this file as part of a free software
+ *  library without restriction.  Specifically, if other files instantiate
+ *  templates or use macros or inline functions from this file, or you compile
+ *  this file and link it with other files to produce an executable, this
+ *  file does not by itself cause the resulting executable to be covered by
+ *  the GNU General Public License.  This exception does not however
+ *  invalidate any other reasons why the executable file might be covered by
+ *  the GNU General Public License.
+ *
+ ****************************************************************************
+ */
+
+/* Author: Massimo Torquati <torquati@di.unipi.it> <massimotor@gmail.com>
+ *
+ * Implementation of the Quicksort algorithm. 
+ * Quicksort is a recursive sorting algorithm that operates by repeatedly 
+ * partitioning an unsorted input list into a pair of unsorted sub-lists, 
+ * such that all of the elements in one of the sub-lists are strictly greater 
+ * than the elements of the other, and then recursively invoking itself on 
+ * the two unsorted sub-lists.
+ * 
+ * This is just a naive implementation of the algorithm, without any specific 
+ * optimisation.
+ *
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ff/utils.hpp>
+
+int size=0;            // array size
+unsigned int *A=NULL;  // array which needs to be ordered
+
+
+void print_array() {
+    int j=0;
+    for(int i=0;i<size;i++) {
+	if (j == 15) {
+	    printf("\n");
+	    j=0;
+	}
+	j++;
+	printf("%d ", A[i]);
+    }
+    printf("\n\n");
+}
+
+
+inline void swap(int i, int j) {
+  register int tmp;  
+  tmp = A[i]; A[i] = A[j]; A[j] = tmp;
+}
+
+
+/* Return the largest of first two keys. */
+inline int FindPivot(int i, int j) {
+    register int pivot = (i+(j-i))/2;
+    if (A[i]>A[pivot]) {
+	if (A[i]>A[j]) return i;
+	else return j;
+    } else {
+	if (A[pivot]>A[j]) return pivot;
+	else return j;
+    }
+    
+    // if (A[i] > A[i+1]) return i;
+    //  else  return i+1;
+}
+			
+
+/* Partition the array between elements i and j around the specified pivot. */
+inline int Partition(int i, int j, unsigned int pivot) {
+    int left = i;
+    int right = j;
+  
+    do {
+        swap(left,right);
+        while (A[left]  <  pivot) left++;
+        while (A[right] >= pivot) right--;
+    } while (left <= right);
+    
+    return(left);
+}
+
+
+		
+inline void QuickSort(int i, int j) {
+    if (j-i < 2) {
+        if (A[i]>A[j]) swap(i,j);
+        return;
+    } 
+    int pivot = FindPivot(i,j);
+    int k     = Partition(i,j,A[pivot]);
+    QuickSort(i, k-1);
+    QuickSort(k,j);
+}
+
+
+// initialise the array A with unique elementes
+void initArray() {
+  int i;
+
+  for (i = 0; i < size; i++) A[i] = i;
+  
+  /* Shuffle them randomly. */
+  srandom(0);
+  for (i = 0; i < size; i++)	
+      swap(i, (random() % (size-i)) + i);
+  
+}
+
+
+
+int main(int argc, char * argv[]) {
+    if (argc!=2) {
+        fprintf(stderr, "use: %s size\n", argv[0]);
+        return -1;
+    }
+    
+    size = atoi(argv[1]);
+    A = new unsigned int[size];
+    if (!A) {
+        fprintf(stdout,"Not enough memory for A\n");
+        exit(1);
+    }
+    
+    initArray();    
+    printf("starting....\n");
+    ff::ffTime(ff::START_TIME);
+    QuickSort(0, size-1);
+    ff::ffTime(ff::STOP_TIME);
+    printf("Time: %g (ms)\n", ff::ffTime(ff::GET_TIME));
+    
+    if (0) print_array();
+    
+    delete [] A;
+    return 0;
+}
+
+
