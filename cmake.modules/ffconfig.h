7eca231848ced6f49a6a838982575308f7d6ba85
aldinuc <aldinuc@gmail.com>
2013-12-28 17:42:23 +0000

    Fixes ro compile C++11 code under MSVC10

--- cmake.modules/ffconfig.h
+++ cmake.modules/ffconfig.h
@@ -1,5 +1,28 @@
-/*
+/* standard headers */
+/* #undef HAVE_MALLOC_H */
+/* #undef HAVE_NUMA_H */
+#define HAVE_STDINT_H
+#define HAVE_PTHREAD_H
 
-This file will be overwritten by cmake 
+/* Compiler type */
+/* #undef CMAKE_COMPILER_IS_GNUCC */
+/* #undef HAS_GCC */
+/* #undef CMAKE_COMPILER_IS_GNUCXX */
+/* #undef HAS_GXX */
+#define HAS_CLANGXX
+/* #undef HAS_MSVC */
+/* #undef HAS_MSVC10 */
 
-*/
+/* cxx11*/
+#define HAS_CXX11_AUTO
+#define HAS_CXX11_NULLPTR
+#define HAS_CXX11_LAMBDA
+#define HAS_CXX11_STATIC_ASSERT
+#define HAS_CXX11_RVALUE_REFERENCES
+#define HAS_CXX11_DECLTYPE
+#define HAS_CXX11_CSTDINT_H
+#define HAS_CXX11_LONG_LONG
+#define HAS_CXX11_VARIADIC_TEMPLATES
+#define HAS_CXX11_CONSTEXPR
+#define HAS_CXX11_SIZEOF_MEMBER
+#define HAS_CXX11_FUNC
