98de5d49d6847d48d24120baba16b3450dc3f365
massimot <massimot@5b2d11e7-8b83-46bf-b187-68a94c616a7d>
2014-03-23 15:57:30 +0000

    - moved to 2.0.4 - added stencilReduceCUDA.hpp - added CUDA tests

--- ff/version.h
+++ ff/version.h
@@ -3,7 +3,7 @@
 
 #define FF_MAJOR_VERSION 2
 #define FF_MINOR_VERSION 0
-#define FF_BETA_VERSION  3
-#define FF_VERSION "2.0.3"
+#define FF_BETA_VERSION  4
+#define FF_VERSION "2.0.4"
 
 #endif /* FF_VERSION_HPP */
