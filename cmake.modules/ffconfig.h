2e41349d31f775bc8a61bb41de59bff7cdf215f9
aldinuc <aldinuc@gmail.com>
2014-04-30 14:56:22 +0000

    Fixes for clang 5.1.

--- cmake.modules/ffconfig.h
+++ cmake.modules/ffconfig.h
@@ -1,17 +1,17 @@
 /* standard headers */
-#define HAVE_MALLOC_H 1
-#define HAVE_NUMA_H
+/* #undef HAVE_MALLOC_H */
+/* #undef HAVE_NUMA_H */
 #define HAVE_STDINT_H
 #define HAVE_PTHREAD_H
-#define HAVE_OMP_H
+/* #undef HAVE_OMP_H */
 
 /* Compiler type */
-#define CMAKE_COMPILER_IS_GNUCC
-#define HAS_GCC                   /* Alias */
-#define CMAKE_COMPILER_IS_GNUCXX 
+/* #undef CMAKE_COMPILER_IS_GNUCC */
+/* #undef HAS_GCC */
+/* #undef CMAKE_COMPILER_IS_GNUCXX */
 /* #undef HAS_GXX */
-/* #undef HAS_CLANGXX */
-#define HAS_MSVC
+#define HAS_CLANGXX
+/* #undef HAS_MSVC */
 /* #undef HAS_MSVC10 */
 
 /* cxx11*/
