6d4657cb585ec325421a8079e418f7d04fd586dd
Massimo Torquati <torquati@di.unipi.it>
2014-07-28 14:16:40 +0000

    added licence file  and added an header to each file containing the licence statement

--- examples/denoiser/src/fastflow/spd/NoiserSPD.hpp
+++ examples/denoiser/src/fastflow/spd/NoiserSPD.hpp
@@ -1,3 +1,33 @@
+/* ***************************************************************************
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ ****************************************************************************
+ */
+
+/*
+ *
+ *  Authors:
+ *    Maurizio Drocco
+ *    Guilherme Peretti Pezzi 
+ *  Contributors:  
+ *    Marco Aldinucci
+ *    Massimo Torquati
+ *
+ *  First version: February 2014
+ */
+
 #ifndef _NOISERSPD_HPP_
 #define _NOISERSPD_HPP_
 
