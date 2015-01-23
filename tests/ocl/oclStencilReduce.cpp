7c12db3e69482440503a77c65112091d622a2da6
drocco <drocco@5b2d11e7-8b83-46bf-b187-68a94c616a7d>
2015-01-23 15:16:37 +0000

    added test for debugging ff_oclStencilReduceOCL

--- tests/ocl/oclStencilReduce.cpp
+++ tests/ocl/oclStencilReduce.cpp
@@ -28,10 +28,13 @@
  *         torquati@di.unipi.it  massimotor@gmail.com
  */
 
+#include <ff/pipeline.hpp>
 #include <ff/stencilReduceOCL.hpp>
 using namespace ff;
 
+#define SIZE 1024
 #define NITER 5
+#define STREAMLEN 10
 
 FFREDUCEFUNC(reducef, float, x, y, return (x+y););
 
@@ -43,6 +46,11 @@ struct oclTask: public baseOCLTask<oclTask, float, float, int, int> {
 	}
 	oclTask(float *M_in_, float *M_out_, int *env_, size_t size_) :
 			M_in(M_in_), M_out(M_out_), size(size_), env(env_) {
+	}
+	~oclTask() {
+		delete[] M_in;
+		delete[] M_out;
+		delete[] env;
 	}
 	void setTask(const oclTask *t) {
 		assert(t);
@@ -62,29 +70,87 @@ struct oclTask: public baseOCLTask<oclTask, float, float, int, int> {
 	int *env;
 };
 
+class Emitter: public ff_node {
+public:
+	Emitter(size_t size_) :
+			n(0), size(size_) {
+		for (int i = 0; i < STREAMLEN; ++i) {
+			float *M_in = new float[size];
+			int *env = new int[size];
+			for (size_t j = 0; j < size; ++j) {
+				M_in[j] = j + 1.0;
+				env[j] = j * 10;
+			}
+			float *M_out = new float[size];
+			tasks[i] = new oclTask(M_in, M_out, env, size);
+		}
+	}
+
+	~Emitter() {
+		for (int i = 0; i < STREAMLEN; ++i) {
+			delete tasks[i];
+		}
+	}
+
+	virtual void *svc(void *task) {
+		if (n < STREAMLEN)
+			return tasks[n++];
+		return EOS;
+	}
+
+private:
+	size_t n, size;
+	oclTask *tasks[STREAMLEN];
+};
+
+class Worker: public ff_node
+//: public ff_stencilReduceOCL<oclTask>
+{
+public:
+//	Worker() :
+//			ff_stencilReduceOCL<oclTask>(mapf, reducef) {
+//	}
+
+	virtual void *svc(void *task) {
+		oclTask *t = (oclTask *) task;
+		//ff_stencilReduceOCL<oclTask>::svc(t);
+		for(size_t i=0; i<t->size; ++i) {
+			t->M_out[i] = t->M_in[i] + t->env[i];
+		}
+		printf("arraysize = %ld\n", t->size);
+		printf("res[%d]=%.2f\n", t->size / 2, t->M_out[t->size / 2]);
+		//printf("reduceVar=%.2f\n", oclt.getReduceVar());
+		//delete t;
+		return GO_ON;
+	}
+};
+
 int main(int argc, char * argv[]) {
-	size_t size = 1024;
+	//one-shot
+	size_t size = SIZE;
 	if (argc > 1)
 		size = atol(argv[1]);
 	printf("arraysize = %ld\n", size);
-
 	float *M_in = new float[size];
 	int *env = new int[size];
 	for (size_t j = 0; j < size; ++j) {
 		M_in[j] = j + 1.0;
-		env[j] = j*10;
+		env[j] = j * 10;
 	}
-
 	float *M_out = new float[size];
-
 	oclTask oclt(M_in, M_out, env, size);
 	ff_stencilReduceOCL<oclTask> oclStencilReduceOneShot(oclt, mapf, reducef);
 	oclStencilReduceOneShot.run_and_wait_end();
+	printf("res[%d]=%.2f\n", size / 2, M_out[size / 2]);
+	//printf("reduceVar=%.2f\n", oclt.getReduceVar());
 
-	delete[] M_in;
-	delete[] M_out;
-	delete[] env;
-	printf("res[%d]=%.2f\n", size/2, M_out[size/2]);
-	printf("reduceVar=%.2f\n", oclt.getReduceVar());
+	//stream
+	Emitter e(size);
+	ff_pipeline pipe;
+	pipe.add_stage(&e);
+	//pipe.add_stage(new Worker());
+	pipe.add_stage(new ff_stencilReduceOCL<oclTask>(mapf, reducef));
+	pipe.run_and_wait_end();
+
 	return 0;
 }
