a0fc61f5501e08e4ff69251ecd68bcfcdfea3272
Maurizio Drocco <maurizio.drocco@gmail.com>
2015-08-14 02:17:25 +0000

    fixed pointer comparison in stencilReduceOCL.hpp

--- tests/ocl/ctest.h
+++ tests/ocl/ctest.h
@@ -9,11 +9,11 @@
 #define TESTS_OCL_CTEST_H_
 
 
-#define SET_DEVICE_TYPE(obj) 
-#define NACC 1
-
-//#define SET_DEVICE_TYPE(obj) obj.pickCPU();
+//#define SET_DEVICE_TYPE(obj) obj.pickGPU();
 //#define NACC 1
+
+#define SET_DEVICE_TYPE(obj) obj.pickCPU();
+#define NACC 1
 
 
 #endif /* TESTS_OCL_CTEST_H_ */
