9d55769f98bf313acc7f9e6f5db5b89980fd2f35
Massimo Torquati <torquati@di.unipi.it>
2015-08-25 07:28:18 +0000

    oclMap tests revisited

--- tests/ocl/oclMap2.cpp
+++ tests/ocl/oclMap2.cpp
@@ -25,24 +25,26 @@
  ****************************************************************************
  */
 /* Author: Massimo Torquati
- *         torquati@di.unipi.it  massimotor@gmail.com
+ *         torquati@di.unipi.it
  */
 // This is the same of the oclMap test but the source code is loaded from a file.
 // The compiled version of the OpenCL program is saved in binary file (.bin) 
-// so that next times the program is not recompiled.
+// so that at the next run the program is not recompiled.
 
 
 #if !defined(FF_OPENCL)
+// needed to enable the OpenCL FastFlow run-time
 #define FF_OPENCL
 #endif
 
 #include <ff/stencilReduceOCL.hpp>
-
 using namespace ff;
 
 #define CHECK 1
 #ifdef CHECK
 #include "ctest.h"
+#else
+#define NACC 1
 #endif
 
 
@@ -50,7 +52,6 @@ struct oclTask: public baseOCLTask<oclTask, float> {
     oclTask():M(NULL),size(0) {}
     oclTask(float *M, size_t size):M(M),size(size) {}
     void setTask(const oclTask *t) { 
-        assert(t);
         setInPtr(t->M, t->size);
         setOutPtr(t->M);
     }
@@ -64,21 +65,32 @@ int main(int argc, char * argv[]) {
     if(argc>1) size     =atol(argv[1]);
     printf("arraysize = %ld\n", size);
 
-    float *M        = new float[size];
-    for(size_t j=0;j<size;++j) M[j]=j;
-
 #ifdef CHECK
-    float *M_        = new float[size];
-    memcpy(M_, M, size * sizeof(float));
+    std::vector<float> M_(size);
+    for(size_t j=0;j<size;++j) M_[j]=j;
 #endif
 
-    oclTask oclt(M, size);
-    ff_mapOCL_1D<oclTask> oclMap(oclt, "cl_code/oclMap.cl","mapf",nullptr,NACC);
-    SET_DEVICE_TYPE(oclMap);
+    std::vector<float> M(size);
 
-    oclMap.saveBinaryFile(); // save the compiled version in cl_code/oclMap.cl.bin
-    oclMap.reuseBinaryFile(); // if the binary file is present it will be used
-    oclMap.run_and_wait_end();
+    oclTask oclt(const_cast<float*>(M.data()), size);
+    ff_mapOCL_1D<oclTask> oclMap1(oclt, "cl_code/oclMap.cl","mapf1",nullptr,NACC);
+    SET_DEVICE_TYPE(oclMap1);
+
+    oclMap1.saveBinaryFile();  // save the compiled version in cl_code/oclMap.cl.bin
+    oclMap1.reuseBinaryFile(); // if the binary file is present it will be used
+    if (oclMap1.run_and_wait_end()<0) {
+        error("running oclMap1\n");
+        return -1;
+    }
+    
+    ff_mapOCL_1D<oclTask> oclMap2(oclt, "cl_code/oclMap.cl","mapf2",nullptr,NACC);
+    SET_DEVICE_TYPE(oclMap2);
+    oclMap2.reuseBinaryFile(); 
+    if (oclMap2.run_and_wait_end()<0) {
+        error("running oclMap2\n");
+        return -1;
+    }
+
 
 #if defined(CHECK)
 	for (size_t i = 0; i < size; ++i) {
@@ -88,9 +100,6 @@ int main(int argc, char * argv[]) {
         }
 	}
     printf("Result correct\n");
-    delete [] M_;
 #endif
-
-    delete [] M;
     return 0;
 }
