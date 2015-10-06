9f14e11892b5756f8332652605693035b9f2e5c4
Massimo Torquati <torquati@di.unipi.it>
2015-10-06 13:23:24 +0000

    tests

---
+++ tests/ocl/dotProduct2.cpp
@@ -0,0 +1,126 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* ***************************************************************************
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as 
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  As a special exception, you may use this file as part of a free software
+ *  library without restriction.  Specifically, if other files instantiate
+ *  templates or use macros or inline functions from this file, or you compile
+ *  this file and link it with other files to produce an executable, this
+ *  file does not by itself cause the resulting executable to be covered by
+ *  the GNU General Public License.  This exception does not however
+ *  invalidate any other reasons why the executable file might be covered by
+ *  the GNU General Public License.
+ *
+ ****************************************************************************
+ */
+/*  This program computes the dot-product of 2 arrays as:
+ *
+ *    s = sum_i(A[i]*B[i])
+ *
+ */
+
+#include <iostream>
+#include <iomanip>
+#if !defined(FF_OPENCL)
+#define FF_OPENCL
+#endif
+#include <ff/stencilReduceOCL.hpp>
+using namespace ff;
+
+#define CHECK 1
+
+FF_OCL_STENCIL_ELEMFUNC_ENV(mapf, float, size, i, A, float, B, { 
+                            (void)size; 
+                            return A[i] * B[i];
+    });
+FF_OCL_REDUCE_COMBINATOR(reducef, float, x, y, {
+        return (x+y);
+    });
+
+struct Task: public baseOCLTask<Task, float, float> {
+	Task(): result(0.0) { }
+	Task(const std::vector<float> &A, const std::vector<float> &B) :
+			A(A), B(B), M(A.size()), result(0.0) {}
+
+	void setTask(const Task *t) {
+
+		setInPtr(const_cast<float*>(t->A.data()), t->A.size(), 
+                 BitFlags::COPYTO,
+                 BitFlags::DONTREUSE,
+                 BitFlags::RELEASE);
+		setEnvPtr(const_cast<float*>(t->B.data()), t->B.size(), 
+                 BitFlags::COPYTO,
+                 BitFlags::DONTREUSE,
+                 BitFlags::RELEASE);
+		setOutPtr(const_cast<float*>(t->M.data()), t->M.size(), 
+                  BitFlags::COPYBACK,
+                  BitFlags::DONTREUSE,
+                  BitFlags::RELEASE);
+		setReduceVar(&(t->result));
+	}
+
+	void setResult(const float r) {result = r;}
+
+	float combinator(float const &x, float const &y) {return x+y;}
+
+	std::vector<float> A, B, M;
+	float result;
+};
+
+int main(int argc, char * argv[]) {
+	size_t arraySize = 1024;
+	if (argc > 2) {
+		std::cerr << "use: " << argv[0] << " size\n";
+		return -1;
+	}
+	if (argc > 1) arraySize = atol(argv[1]);
+
+	std::vector<float> A(arraySize), B(arraySize);
+
+	for (size_t i = 0; i < arraySize; ++i) {
+		A[i] = 1.0;
+		B[i] = i;
+	}
+    
+	std::vector<cl_device_id> dev;
+    int tmp = clEnvironment::instance()->getGPUDevice();
+    // if a gpu device does not exist, switching to CPU
+    if (tmp == -1) 
+        tmp = clEnvironment::instance()->getCPUDevice();
+	dev.push_back(clEnvironment::instance()->getDevice(tmp)); //convert logical to OpenCL Id
+
+    Task task(A, B);
+	ff_mapReduceOCL_1D<Task> oclmap(task, mapf, reducef, 0.0);
+	oclmap.setDevices(dev);
+    if (oclmap.run_and_wait_end()<0) {
+        error("running oclmap\n");
+        return -1;
+    }
+
+	std::cout << "Result = " << std::setprecision(10) << task.result << "\n";
+
+#if defined(CHECK)
+	float r = 0.0;
+	for (size_t j = 0; j < arraySize; ++j) {
+		r += A[j] * B[j];
+	}
+	if (r != task.result) {
+		printf("Wrong result, should be %.2f\n", r);
+		exit(1); //ctest
+	} else
+		printf("OK\n");
+#endif
+
+	return 0;
+}
---
+++ tests/ocl/helloKernel2.cpp
@@ -0,0 +1,199 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* ***************************************************************************
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as 
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  As a special exception, you may use this file as part of a free software
+ *  library without restriction.  Specifically, if other files instantiate
+ *  templates or use macros or inline functions from this file, or you compile
+ *  this file and link it with other files to produce an executable, this
+ *  file does not by itself cause the resulting executable to be covered by
+ *  the GNU General Public License.  This exception does not however
+ *  invalidate any other reasons why the executable file might be covered by
+ *  the GNU General Public License.
+ *
+ ****************************************************************************
+ */
+/* Author: Massimo Torquati
+ *         (September 2015)       
+ *  
+ */
+
+#if !defined(FF_OPENCL)
+#define FF_OPENCL
+#endif
+
+#include <vector>
+#include <iostream>
+#include <ff/map.hpp>
+#include <ff/stencilReduceOCL.hpp>
+#include <ff/selector.hpp>
+#include <ff/taskf.hpp>
+
+using namespace ff;
+
+#define CHECK 1
+#ifdef CHECK
+#include "ctest.h"
+#else
+#define NACC 1
+#endif
+
+//#define SYNCHRONOUS_EXECUTION
+const size_t DEFAULT_ARRAYSIZE=1024;
+
+
+FF_OCL_MAP_ELEMFUNC(mapf, float, useless, i,
+                    (void)useless;
+                    return (i + 1/(i+1));
+                    );
+
+
+// this is the task
+struct myTask {
+    myTask(std::vector<float> &A):A(A) {}
+    std::vector<float> &A;
+};
+
+// oclTask is used to transfer the input task to/from the OpenCL device
+// - myTask is the type of the input task
+// - float* is the type of OpenCL input array
+// - float* is the type of OpenCL output array
+struct oclTask: baseOCLTask<myTask, float, float> {
+    void setTask(const myTask *task) { 
+
+        std::cout << "running on GPU\n";
+
+        float *Aptr         = const_cast<float*>(task->A.data());
+        const size_t Asize  = task->A.size();
+
+        // A is not copied in input (only allocated), it will be deleted at the end
+        setInPtr(Aptr, Asize, 
+                 BitFlags::DONTCOPYTO,
+                 BitFlags::DONTREUSE,
+                 BitFlags::RELEASE);
+        
+        // A is copied back at the end, it will be deleted at the end
+        setOutPtr(Aptr, Asize, 
+                  BitFlags::COPYBACK,
+                  BitFlags::DONTREUSE,
+                  BitFlags::RELEASE);
+    }
+};
+
+// CPU map
+struct cpuMap: ff_Map<myTask> {
+    myTask *svc(myTask *in) {
+        std::vector<float> &A = in->A;
+
+        std::cout << "running on CPU\n";
+        
+        ff_Map::parallel_for(0,A.size(),[&A](const long i) {
+                A[i] = (i + 1/(i+1));
+            });    
+        return in;
+    }
+};
+
+// OpenCL map
+struct oclMap: ff_mapOCL_1D<myTask, oclTask> {
+    oclMap(const std::string &path, const std::string &kname): ff_mapOCL_1D<myTask, oclTask>(path, kname,nullptr,NACC) {}
+    oclMap(): ff_mapOCL_1D<myTask, oclTask>(mapf,nullptr,NACC) {}
+};
+
+// selector, it decides whether to execute the CPU or OpenCL map
+struct rprKernel: ff_nodeSelector<myTask> {
+    rprKernel(int kernelId):kernelId(kernelId) {} 
+
+    myTask *svc(myTask *in) {
+	
+        // make the decision 
+        int selectedDevice = kernelId; 
+
+        selectNode(selectedDevice);
+        return ff_nodeSelector<myTask>::svc(in);
+    }
+
+    int kernelId;
+};
+
+
+int main(int argc, char *argv[]) {
+    size_t size = DEFAULT_ARRAYSIZE;
+    if (argc > 1) size = atol(argv[1]);
+    
+    std::vector<float> A0(size), A1(size);
+    
+    // creates the instances of the CPU map
+    cpuMap cpumap0, cpumap1;
+    // creates the instances of the GPU map
+    oclMap oclmap0, oclmap1;
+
+    // defines the 2 kernels providing the unique kernel id
+    rprKernel kernel0(0); 
+    rprKernel kernel1(1); 
+
+    // adds the CPU and GPU instances to the kernel 0
+    kernel0.addNode(cpumap0);
+    kernel0.addNode(oclmap0);
+
+    // adds the CPU and GPU instances to the kernel 1
+    kernel1.addNode(cpumap1);
+    kernel1.addNode(oclmap1);
+
+    // executes initialization of each kernels
+    if (kernel0.nodeInit()<0) {
+        error("cannot initialize kernel0\n");
+        return -1;
+    }
+    if (kernel1.nodeInit()<0) {
+        error("cannot initialize kernel1\n");
+        return -1;
+    }
+    
+    // defines 2 lambdas that run the kernels
+    auto F1 = [&]() { myTask task(A0); kernel0.svc(&task); };
+    auto F2 = [&]() { myTask task(A1); kernel1.svc(&task); };
+
+    // creates the asynchronous patterns with 2 executors
+    ff_taskf taskf(2); 
+
+    // adds the lambdas to the scheduler
+    taskf.AddTask(F1); 
+    taskf.AddTask(F2);
+
+    // starts the execution of the scheduler waiting for the termination
+    if (taskf.run_and_wait_end()<0) {
+        error("error running taskf\n");
+        return -1;
+    }
+        
+
+#if defined(CHECK)
+    bool wrong = false;
+    for(size_t i=0;i<A0.size(); ++i) {
+        if (A0[i] != (i + 1/(i+1))) {
+            std::cerr << "Wrong result, A0[" << i <<"], expected " << (i+1/(i+1)) << " obtained "<< A0[i] << "\n";
+            wrong = true;
+        }
+        if (A1[i] != (i + 1/(i+1))) {
+            std::cerr << "Wrong result, A1[" << i <<"], expected " << (i+1/(i+1)) << " obtained "<< A1[i] << "\n";
+            wrong = true;
+        }
+
+    }
+    if (!wrong) std::cerr << "The result is OK\n";
+    else exit(1); //ctest
+#endif
+    return 0;
+}
