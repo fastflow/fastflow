9ead06cd19989871e13b3dd7b69badcfd9a58adc
massimot <massimot@5b2d11e7-8b83-46bf-b187-68a94c616a7d>
2015-10-07 15:34:23 +0000

    tests

--- examples/OpenCVvideo/ffvideo.cpp
+++ examples/OpenCVvideo/ffvideo.cpp
@@ -44,39 +44,41 @@ struct Source : ff_node_t<cv::Mat> {
     Source(const std::string filename):filename(filename) {}
   
     cv::Mat * svc(cv::Mat *) {
-        //VideoCapture cap(filename.c_str()); 
-        VideoCapture cap(1); 
-	if(!cap.isOpened())  {  
-	    std::cout << "Error opening input file" << std::endl;
-	    return EOS;
-	} 
-	for(;;) {
-	    Mat * frame = new Mat();
-	    if(cap.read(*frame))  ff_send_out(frame);
-	    else {
-		std::cout << "End of stream in input" << std::endl; 
-		break;
-	    }
-	}
-	return EOS;
+        VideoCapture cap(filename.c_str()); 
+        //VideoCapture cap(1); 
+        if(!cap.isOpened())  {  
+            std::cout << "Error opening input file" << std::endl;
+            return EOS;
+        } 
+        for(;;) {
+            Mat * frame = new Mat();
+            if(cap.read(*frame))  ff_send_out(frame);
+            else {
+                std::cout << "End of stream in input" << std::endl; 
+                break;
+            }
+        }
+        return EOS;
     }
 }; 
 
 // this stage apply the GaussianBlur filter and sends the result to the next stage
 struct Stage1 : ff_node_t<cv::Mat> {
+
     cv::Mat * svc(cv::Mat *frame) {
-	Mat frame1;
-	cv::GaussianBlur(*frame, frame1, cv::Size(0, 0), 3);
-	cv::addWeighted(*frame, 1.5, frame1, -0.5, 0, *frame);
-	return frame;
-  }
+        Mat frame1;
+        cv::GaussianBlur(*frame, frame1, cv::Size(0, 0), 3);
+        cv::addWeighted(*frame, 1.5, frame1, -0.5, 0, *frame);
+        return frame;
+    }
 }; 
 
 // this stage apply the Sobel filter and sends the result to the next stage
 struct Stage2 : ff_node_t<cv::Mat> {
+
     cv::Mat * svc(cv::Mat *frame) {
-	Sobel(*frame,*frame,-1,1,0,3);
-	return frame;
+        Sobel(*frame,*frame,-1,1,0,3);
+        return frame;
     }
 }; 
 
@@ -85,17 +87,17 @@ struct Drain: ff_node_t<cv::Mat> {
     Drain(bool ovf):outvideo(ovf) {}
 
     int svc_init() {
-	if(outvideo) namedWindow("edges",1);
-	return 0; 
+        if(outvideo) namedWindow("edges",1);
+        return 0; 
     }
-
+    
     cv::Mat *svc (cv::Mat * frame) {
-	if(outvideo) {
-	    imshow("edges", *frame);
-	    waitKey(30);    // why 30ms here ?
-	} 
-	delete frame;
-	return GO_ON;
+        if(outvideo) {
+            imshow("edges", *frame);
+            waitKey(30);    // why 30ms here ?
+        } 
+        delete frame;
+        return GO_ON;
     }
 protected:
     const bool outvideo; 
@@ -115,20 +117,20 @@ int main(int argc, char *argv[]) {
     // filter parameters
     bool filter1, filter2;
     if(atoi(argv[2]) == 0) {
-	filter1 = false; filter2 = false;
-	std::cout << "No filters" << std::endl; 
+        filter1 = false; filter2 = false;
+        std::cout << "No filters" << std::endl; 
     }
     if(atoi(argv[2]) == 1) { 
-	filter1 = true;  filter2 = false; 
-	std::cout << "Applying enhnace filter only" << std::endl;
+        filter1 = true;  filter2 = false; 
+        std::cout << "Applying enhnace filter only" << std::endl;
     }
     if(atoi(argv[2]) == 2) { 
-	filter1 = false; filter2 = true;     
-	std::cout << "Applying emboss filter only" << std::endl;
+        filter1 = false; filter2 = true;     
+        std::cout << "Applying emboss filter only" << std::endl;
     }
     if(atoi(argv[2]) == 3) { 
-	filter1 = true;  filter2 = true;     
-	std::cout << "Applying both filters" << std::endl;
+        filter1 = true;  filter2 = true;     
+        std::cout << "Applying both filters" << std::endl;
     }
     // output 
     bool outvideo = false; 
@@ -147,54 +149,54 @@ int main(int argc, char *argv[]) {
     // adds the second stage (sequential or ordered farm)
     if (filter1) {
       if (nw1 == 1) 
-	  // the second stage is sequential
-	  pipe.add_stage(make_unique<Stage1>());
+          // the second stage is sequential
+          pipe.add_stage(make_unique<Stage1>());
       else {
-	  // the second stage is an ordered task-farm 
-	  // by default the ordered farm has the collector
-	  pipe.add_stage(make_unique<ff_OFarm<cv::Mat> >([nw1]() {
-
-		      std::vector<std::unique_ptr<ff_node> > W; 
-		      for(size_t i=0; i<nw1; i++) 
-			  W.push_back(make_unique<Stage1>());
-		      return W;
-
-		  } ()
-		  ));	  
+          // the second stage is an ordered task-farm 
+          // by default the ordered farm has the collector
+          pipe.add_stage(make_unique<ff_OFarm<cv::Mat> >([nw1]() {
+                      
+                      std::vector<std::unique_ptr<ff_node> > W; 
+                      for(size_t i=0; i<nw1; i++) 
+                          W.push_back(make_unique<Stage1>());
+                      return W;
+                      
+                  } ()
+                  ));	  
       }
     }
-
+    
     // adds the third stage (sequential or ordered farm)
     if (filter2) {
-      if (nw2 == 1) 
-	  // the third stage is sequential
-	  pipe.add_stage(make_unique<Stage2>());
-      else {
-	  // the third stage is an ordered task-farm 
-	  // by default the ordered farm has the collector
-	  pipe.add_stage(make_unique<ff_OFarm<cv::Mat> >([nw2]() {
-
-		      std::vector<std::unique_ptr<ff_node> > W;
-		      for(size_t i=0; i<nw2; i++) 
-			  W.push_back(make_unique<Stage2>());
-		      return W;
-
-		  } ()
-		  ));
-      }
+        if (nw2 == 1) 
+            // the third stage is sequential
+            pipe.add_stage(make_unique<Stage2>());
+        else {
+            // the third stage is an ordered task-farm 
+            // by default the ordered farm has the collector
+            pipe.add_stage(make_unique<ff_OFarm<cv::Mat> >([nw2]() {
+                        
+                        std::vector<std::unique_ptr<ff_node> > W;
+                        for(size_t i=0; i<nw2; i++) 
+                            W.push_back(make_unique<Stage2>());
+                        return W;
+                        
+                    } ()
+                    ));
+        }
     }
     
     // adds the last stage 
     pipe.add_stage(make_unique<Drain>(outvideo));
-
+    
     ffTime(START_TIME);
     // starts the pipeline and waits for termination
     if (pipe.run_and_wait_end()<0) {
-	error("running pipeline");
-	return -1;
+        error("running pipeline");
+        return -1;
     }
     ffTime(STOP_TIME);
-
+    
     std::cout << "Elapsed (seq,"; 
     if(nw1 == 1)  std::cout << "seq,"; 
     else    	  std::cout << "farm(" << nw1 << "),";
