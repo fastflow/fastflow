6dcc3410357b754a11989e6c2b9141339385325c
massimot <massimot@5b2d11e7-8b83-46bf-b187-68a94c616a7d>
2016-01-27 06:58:19 +0000

    moved to 2.1.1

--- ff/version.h
+++ ff/version.h
@@ -3,7 +3,7 @@
 
 #define FF_MAJOR_VERSION 2
 #define FF_MINOR_VERSION 1
-#define FF_BETA_VERSION  0
-#define FF_VERSION "2.1.0"
+#define FF_BETA_VERSION  1
+#define FF_VERSION "2.1.1"
 
 #endif /* FF_VERSION_HPP */
