e619e92c2c953acb0ff20c84d36d8fb3901b8020
Maurizio Drocco <drocco@di.unito.it>
2018-02-12 18:16:14 -0500

    commented gff_bb_farm test

--- tests/d/gff/gff_bb_farm.cpp
+++ tests/d/gff/gff_bb_farm.cpp
@@ -44,7 +44,6 @@
 #include <cmath>
 
 #include <ff/d/gff/gff.hpp>
-#include <TrackingAllocator.hpp>
 
 #define NWORKERS                 4
 #define STREAMLEN             1024
@@ -52,109 +51,167 @@
 #define THRESHOLD  (RNG_LIMIT / 2)
 
 /*
- ***************************************************************************
+ * To define a gff node, the user has to define first an internal class (a.k.a.
+ * dff logic), with the following functions:
+ * - svc_init: called once at the beginning of node's execution
+ * - svc_end:  called once at the end of node's execution
+ * - svc:      called repeatedly during node's execution
+ *
+ * The signature of svc varies depending on node's family (source, processor,
+ * sink). See sample codes.
+ *
+ * To complete the definition of a gff node, the internal class is passed as
+ * template parameter to the generic class corresponding to the node's family.
+ */
+
+/*
+ *******************************************************************************
  *
  * farm components
  *
- ***************************************************************************
+ *******************************************************************************
  */
 /*
- * Source generates a stream of random integers within [0, RNG_LIMIT)
+ * gff logic generating a stream of random integers
  */
 class EmitterLogic {
 public:
-    unsigned n = 0;
-    std::mt19937 rng;
+	EmitterLogic() :
+			dist(0, RNG_LIMIT) {
+	}
 
-    gff::token_t svc(gff::RoundRobinSwitch &c)
-    {
-        if (n++ < STREAMLEN)
-        {
-            c.emit(gam::make_private<int>((int) (rng() % RNG_LIMIT)));
-            return gff::go_on;
-        }
-        return gff::eos;
-    }
+	/**
+	 * The svc function is called repeatedly by the runtime, until an eos
+	 * token is returned.
+	 * Pointers are sent downstream by calling the emit() function on the
+	 * output channel, that is passed as input argument.
+	 *
+	 * @param c is the output channel (could be a template for simplicity)
+	 * @return a gff token
+	 */
+	gff::token_t svc(gff::RoundRobinSwitch &c) {
+		if (n++ < STREAMLEN) {
+			c.emit(gam::make_private<int>(dist(rng)));
+			return gff::go_on;
+		}
+		return gff::eos;
+	}
 
-    void svc_init()
-    {
-    }
+	void svc_init() {
+	}
 
-    void svc_end()
-    {
-    }
+	void svc_end() {
+	}
+
+private:
+	unsigned n = 0;
+	std::mt19937 rng;
+	std::uniform_int_distribution<int> dist;
 };
 
+/*
+ * define a Source node with the following template parameters:
+ * - the type of the output channel
+ * - the type of the emitted pointers
+ * - the gff logic
+ */
 typedef gff::Source<gff::RoundRobinSwitch, //
-        gam::private_ptr<int>, //
-        EmitterLogic> Emitter;
+		gam::private_ptr<int>, //
+		EmitterLogic> Emitter;
 
 /*
- * Lowpass selects input integers below THRESHOLD and computes sqrt
+ * gff logic low-passing input integers below THRESHOLD and computes sqrt
  */
 class WorkerLogic {
 public:
-    gff::token_t svc(gam::private_ptr<int> &in, gff::NondeterminateMerge &c)
-    {
-        auto local_in = in.local();
-        if (*local_in < THRESHOLD)
-            c.emit(gam::make_private<char>((char) std::sqrt(*local_in)));
-        return gff::go_on;
-    }
+	/**
+	 * The svc function is called upon each incoming pointer from upstream.
+	 * Pointers are sent downstream by calling the emit() function on the
+	 * output channel, that is passed as input argument.
+	 *
+	 * @param in is the input pointer
+	 * @param c is the output channel (could be a template for simplicity)
+	 * @return a gff token
+	 */
+	gff::token_t svc(gam::private_ptr<int> &in, gff::NondeterminateMerge &c) {
+		auto local_in = in.local();
+		if (*local_in < THRESHOLD)
+			c.emit(gam::make_private<char>((char) std::sqrt(*local_in)));
+		return gff::go_on;
+	}
 
-    void svc_init()
-    {
-    }
+	void svc_init() {
+	}
 
-    void svc_end()
-    {
-    }
+	void svc_end() {
+	}
 };
-
-typedef gff::Filter<gff::RoundRobinSwitch, gff::NondeterminateMerge, //
-        gam::private_ptr<int>, gam::private_ptr<char>, //
-        WorkerLogic> Worker;
 
 /*
- * Collector sums up all filtered tokens
+ * define a Source node with the following template parameters:
+ * - the type of the input channel
+ * - the type of the output channel
+ * - the type of the input pointers
+ * - the type of the output pointers
+ * - the gff logic
+ */
+typedef gff::Filter<gff::RoundRobinSwitch, gff::NondeterminateMerge, //
+		gam::private_ptr<int>, gam::private_ptr<char>, //
+		WorkerLogic> Worker;
+
+/*
+ * gff logic summing up all filtered tokens and finally checking the result
  */
 class CollectorLogic {
-private:
-    int sum = 0;
-    std::mt19937 rng;
-
 public:
-    void svc(gam::private_ptr<char> &in)
-    {
-        auto local_in = in.local();
-        std::cout << (int) *local_in << std::endl;
-        sum += *local_in;
-    }
+	CollectorLogic() : dist(0, RNG_LIMIT) {}
 
-    void svc_init()
-    {
-    }
+	/**
+	 * The svc function is called upon each incoming pointer from upstream.
+	 *
+	 * @param in is the input pointer
+	 * @return a gff token
+	 */
+	void svc(gam::private_ptr<char> &in) {
+		auto local_in = in.local();
+		std::cout << (int) *local_in << std::endl;
+		sum += *local_in;
+	}
 
-    void svc_end()
-    {
-        int res = 0;
-        for (unsigned i = 0; i < STREAMLEN; ++i)
-        {
-            int x = (int) (rng() % RNG_LIMIT);
-            if (x < THRESHOLD)
-                res += (char) std::sqrt(x);
-        }
-        if (res != sum)
-        {
-            fprintf(stderr, "sum=%d exp=%d\n", sum, res);
-            exit(1);
-        }
-    }
+	void svc_init() {
+	}
+
+	/*
+	 * at the end of processing, check the result
+	 */
+	void svc_end() {
+		int res = 0;
+		for (unsigned i = 0; i < STREAMLEN; ++i) {
+			int x = dist(rng);
+			if (x < THRESHOLD)
+				res += (char) std::sqrt(x);
+		}
+		if (res != sum) {
+			fprintf(stderr, "sum=%d exp=%d\n", sum, res);
+			exit(1);
+		}
+	}
+
+private:
+	int sum = 0;
+	std::mt19937 rng;
+	std::uniform_int_distribution<int> dist;
 };
 
+/*
+ * define a Source node with the following template parameters:
+ * - the type of the input channel
+ * - the type of the input pointers
+ * - the gff logic
+ */
 typedef gff::Sink<gff::NondeterminateMerge, //
-        gam::private_ptr<char>, //
-        CollectorLogic> Collector;
+		gam::private_ptr<char>, //
+		CollectorLogic> Collector;
 
 /*
  *******************************************************************************
@@ -163,17 +220,27 @@ typedef gff::Sink<gff::NondeterminateMerge, //
  *
  *******************************************************************************
  */
-int main(int argc, char * argv[])
-{
-    gff::RoundRobinSwitch e2w;
-    gff::NondeterminateMerge w2c;
+int main(int argc, char * argv[]) {
+	/*
+	 * Create the channels for inter-node communication.
+	 * A channel can carry both public and private pointers.
+	 */
+	gff::RoundRobinSwitch e2w;
+	gff::NondeterminateMerge w2c;
 
-    gff::add(Emitter(e2w));
-    for (unsigned i = 0; i < NWORKERS; ++i)
-        gff::add(Worker(e2w, w2c));
-    gff::add(Collector(w2c));
+	/*
+	 * In this preliminary implementation, a single global network is
+	 * created and nodes can be added only to the global network.
+	 */
 
-    gff::run();
+	/* bind nodes to channels and add to the network */
+	gff::add(Emitter(e2w)); //e2w is the emitter's output channel
+	for (unsigned i = 0; i < NWORKERS; ++i)
+		gff::add(Worker(e2w, w2c)); //e2w/w2c are the workers' i/o channels
+	gff::add(Collector(w2c)); //w2c is the collector's input channel
 
-    return 0;
+	/* execute the network */
+	gff::run();
+
+	return 0;
 }
