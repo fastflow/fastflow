b396caa129761f1a633b6b290373f79c0ddb7025
massimotorquati <torquati@di.unipi.it>
2019-01-15 16:52:46 +0100

    new version ready for tagging

--- ff/version.h
+++ ff/version.h
@@ -2,8 +2,8 @@
 #define FF_VERSION_HPP
 
 #define FF_MAJOR_VERSION 2
-#define FF_MINOR_VERSION 1
-#define FF_BETA_VERSION  3
-#define FF_VERSION "2.1.3"
+#define FF_MINOR_VERSION 2
+#define FF_BETA_VERSION  0
+#define FF_VERSION "2.2.0"
 
 #endif /* FF_VERSION_HPP */
