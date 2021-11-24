ef9ff9417172092d3426e48fcf63d9846c2c31df
Massimo Torquati <massimotor@gmail.com>
2021-11-24 19:25:12 +0100

    minor modification to remove some memory leaks

--- ff/distributed/ff_dgroups.hpp
+++ ff/distributed/ff_dgroups.hpp
@@ -32,11 +32,16 @@ class dGroups {
 public:
 
     static dGroups* Instance(){
-        if (i == nullptr)
-            i = new dGroups();
-        return i;
+		static dGroups dg;
+		return &dg;
     }
-
+	~dGroups() {
+		for (auto g : groups)
+            if (g.second)
+				delete g.second;
+		groups.clear();
+	}
+	
     Proto usedProtocol;
 
     void parseConfig(std::string);
