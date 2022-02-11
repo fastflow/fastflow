526ce000f4b35ce48fc9eacc791a8d64b0382f11
Massimo Torquati <massimotor@gmail.com>
2022-02-11 18:41:54 +0100

    Moved to dual license model: LGPLv3 and MIT

--- ff/mapping_string.sh
+++ ff/mapping_string.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 #
+#
+#
+#  FastFlow is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU Lesser General Public License version 3 as
+#  published by the Free Software Foundation.
+#  Starting from version 3.0.1 FastFlow is dual licensed under the GNU LGPLv3
+#  or MIT License (https://github.com/ParaGroup/WindFlow/blob/vers3.x/LICENSE.MIT)
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+#  License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+#
 # Author: Massimo Torquati
 # 
 # Requires: bash >=3.2, hwloc
