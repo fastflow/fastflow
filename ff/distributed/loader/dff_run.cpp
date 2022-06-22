6f9f47fd15c22c31d63961cb7b40d7789ee42393
Massimo Torquati <massimotor@gmail.com>
2022-06-22 18:18:28 +0200

    by-node mapping in MPI

--- ff/distributed/loader/dff_run.cpp
+++ ff/distributed/loader/dff_run.cpp
@@ -306,7 +306,7 @@ int main(int argc, char** argv) {
 
         char command[350];
      
-        sprintf(command, "mpirun -np %lu --hostfile %s %s --DFF_Config=%s", parsedGroups.size(), hostFile.c_str(), executable.c_str(), configFile.c_str());
+        sprintf(command, "mpirun -np %lu --hostfile %s --map-by node -%s --DFF_Config=%s", parsedGroups.size(), hostFile.c_str(), executable.c_str(), configFile.c_str());
 
 		std::cout << "mpicommand: " << command << "\n";
 		
